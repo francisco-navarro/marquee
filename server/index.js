57d56098614a4701e51bf523e9b46a54ac3ef746
Paco Navarro <packarbell@gmail.com>
2017-01-24 08:51:47 +0100

    mongoose

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var proxy = require('express-http-proxy');
 var API = require('./api-routes');
 
 var db = require('../config/db');
+
+mongoose.connect(db.url);
 
 var angularCliPort = 4200;
 var port = process.env.PORT || 3001;
