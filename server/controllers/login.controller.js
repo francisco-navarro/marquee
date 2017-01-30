8df84913bc9fc863726a76fa7fafe5878eb4bf15
Paco Navarro <packarbell@gmail.com>
2017-01-30 15:35:38 +0100

    login and register

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -3,14 +3,25 @@
   const jwt = require('express-jwt');
 
   function init(router) {
-    router.post('/login', post);
+    router.put('/login', login);
+    router.post('/login', register);
   }
 
-  function register(req, res) {
+  function login(req, res) {
     if (!req.body.username || !req.body.password) {
       return res.status(400).json({ message: 'Please fill out all fields' });
     }
     passport.authenticate('local', 
+      { failureRedirect: '/login' },
+      (req, res) => {
+        // if username and password correct 
+        res.redirect('/');
+      }
+    );
+  }
+
+  function register(req, res) {
+    passport.authenticate('local',
       { failureRedirect: '/login' },
       (req, res) => {
         // if username and password correct 
